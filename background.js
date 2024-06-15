57a20ecbde7c98bbbdf284be38099674cd8e435b
tk <taiki.a.k.a.tk@gmail.com>
2024-06-16 00:42:19 +0900

    add: initial version

---
+++ background.js
@@ -0,0 +1,42 @@
+chrome.commands.onCommand.addListener(function(command) {
+  chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+    let activeTab = tabs[0];
+    
+    if (command === "paste-user-id") {
+      console.log("Paste User ID command received");
+      chrome.scripting.executeScript({
+        target: { tabId: activeTab.id },
+        function: pasteUserId
+      });
+    } else if (command === "move-tab-left") {
+      console.log("Moving to the previous tab");
+      chrome.tabs.query({ currentWindow: true }, function(allTabs) {
+        let newIndex = (activeTab.index - 1 + allTabs.length) % allTabs.length;
+        chrome.tabs.update(allTabs[newIndex].id, { active: true });
+      });
+    } else if (command === "move-tab-right") {
+      console.log("Moving to the next tab");
+      chrome.tabs.query({ currentWindow: true }, function(allTabs) {
+        let newIndex = (activeTab.index + 1) % allTabs.length;
+        chrome.tabs.update(allTabs[newIndex].id, { active: true });
+      });
+    }
+  });
+});
+
+function pasteUserId() {
+  console.log("Executing pasteUserId function");
+  navigator.clipboard.readText().then(text => {
+    console.log("Clipboard text: ", text);
+    let textField = document.querySelector("#prompt-textarea"); // ID属性を使用
+    if (textField) {
+      console.log("Text field found");
+      textField.value = text;
+      console.log("Text field value set");
+    } else {
+      console.log("Text field not found");
+    }
+  }).catch(err => {
+    console.error("Failed to read clipboard text: ", err);
+  });
+}

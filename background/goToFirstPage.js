3ca6d11c5281644d4c57812a0e29b34cbacbf4f1
tk <103865144+tk3dots@users.noreply.github.com>
2024-06-21 09:35:01 +0900

    Update goToFirstPage.js

--- background/goToFirstPage.js
+++ background/goToFirstPage.js
@@ -1,9 +1,14 @@
 async function goToFirstPage(tabId) {
-    await chrome.scripting.executeScript({
-      target: { tabId },
-      function: function() {
-        window.history.go(-(window.history.length - 1));
-      }
-    });
-  }
-  
+  console.log("Attempting to go to the first page in tab history");
+  await chrome.scripting.executeScript({
+    target: { tabId },
+    function: function() {
+      console.log("Executing script to go to the first page in tab history");
+      window.history.go(-(window.history.length - 1));
+    }
+  }).then(() => {
+    console.log("Script executed successfully");
+  }).catch((error) => {
+    console.error("Failed to execute script:", error);
+  });
+}

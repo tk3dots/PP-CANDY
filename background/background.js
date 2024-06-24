be167b69f44f1f920d98df9d7f7818fa506c556f
tk <taiki.a.k.a.tk@gmail.com>
2024-06-24 23:51:53 +0900

    refactor: Add keyboard shortcuts for tab navigation in background.js

--- background/background.js
+++ background/background.js
@@ -1,3 +1,38 @@
+importScripts('moveTabs.js', 'pasteDateTime.js', 'goToFirstPage.js');
+
+chrome.commands.onCommand.addListener(async function(command) {
+  const tabs = await chrome.tabs.query({ currentWindow: true, active: true });
+  let activeTab = tabs[0];
+
+  if (command === "move-tab-left") {
+    console.log("Moving to the previous tab");
+    await moveTabLeft();
+  } else if (command === "move-tab-right") {
+    console.log("Moving to the next tab");
+    await moveTabRight();
+  } else if (command === "paste-date-time") {
+    console.log("Paste Date and Time command received");
+    await pasteDateTime(activeTab.id);
+  } else if (command === "go-to-first-page") {
+    console.log("Go to First Page command received");
+    await goToFirstPage(activeTab.id);
+  }
+});
+
+async function moveTabLeft() {
+  const tabs = await chrome.tabs.query({ currentWindow: true });
+  const activeTab = tabs.find(tab => tab.active);
+  const newIndex = (activeTab.index - 1 + tabs.length) % tabs.length;
+  await chrome.tabs.update(tabs[newIndex].id, { active: true });
+}
+
+async function moveTabRight() {
+  const tabs = await chrome.tabs.query({ currentWindow: true });
+  const activeTab = tabs.find(tab => tab.active);
+  const newIndex = (activeTab.index + 1) % tabs.length;
+  await chrome.tabs.update(tabs[newIndex].id, { active: true });
+}
+
 // Create context menu items
 chrome.runtime.onInstalled.addListener(() => {
   // Create a top-level context menu item for storing selected text

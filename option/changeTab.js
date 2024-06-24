9b139f589bce94e2e6300923cdb0b9fa5faeb7a6
tk <taiki.a.k.a.tk@gmail.com>
2024-06-24 18:59:10 +0900

    chore: Remove deprecated options page and associated script

---
+++ option/changeTab.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('tab1-btn').addEventListener('click', function() { changeTab(1); });
+    document.getElementById('tab2-btn').addEventListener('click', function() { changeTab(2); });
+    document.getElementById('tab3-btn').addEventListener('click', function() { changeTab(3); });
+    document.getElementById('tab4-btn').addEventListener('click', function() { changeTab(4); });
+    document.getElementById('tab5-btn').addEventListener('click', function() { changeTab(5); });
+  });
+  
+  function changeTab(tabNumber) {
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    document.getElementById("tab" + tabNumber).style.display = "block";
+  }
